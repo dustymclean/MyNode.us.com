ccfe3c8b0ae92350c77778766574999473cd5590
dustymcLean <dusty_mclean@icloud.com>
2026-02-16 10:11:14 -0600

    Update config for custom domain and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // Sets the base URL for GitHub Pages deployment
-      base: '/MyNode.us.com/',
+      // CHANGED: Base must be '/' for custom domains
+      base: '/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
